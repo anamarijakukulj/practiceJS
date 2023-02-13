3bbcd68dbe42ebf21ebd598fc26aab41c42934eb
Anamarija <97092170+anamarijakukulj@users.noreply.github.com>
2023-02-13 15:16:29 +0100

    Update index.js

--- the-dice-game/index.js
+++ the-dice-game/index.js
@@ -1,1 +1,27 @@
+const imageOne = document.querySelector(".img1");
+const imageTwo = document.querySelector(".img2");
+const result = document.querySelector("h1");
+const play = document.querySelector("button");
+
+const randomNum = () => {
+  return Math.floor(Math.random() * 6 + 1);
+};
+
+const game = () => {
+  let playerOne = randomNum();
+  let playerTwo = randomNum();
+
+  imageOne.src = `images/dice${playerOne}.png`;
+  imageTwo.src = `images/dice${playerTwo}.png`;
+
+  if (playerOne > playerTwo) {
+    result.innerText = "Player 1 win!";
+  } else if (playerOne < playerTwo) {
+    result.innerText = "Player 2 win!";
+  } else {
+    result.innerText = "Tie";
+  }
+};
+
+play.addEventListener("click", game);
 
