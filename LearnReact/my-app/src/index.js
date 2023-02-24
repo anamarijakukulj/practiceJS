16006d86243c36d55f1cd9d65a4e1bcc3ece976a
Anamarija <97092170+anamarijakukulj@users.noreply.github.com>
2023-02-24 13:12:13 +0100

    First component

--- LearnReact/my-app/src/index.js
+++ LearnReact/my-app/src/index.js
@@ -1,1 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
+function Greetings() {
+  return <h2>My First Component</h2>;
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Greetings />);
