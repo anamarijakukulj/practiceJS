94a2469aa6bc520f8cd5b5cbb79659c5ad89178a
Anamarija <97092170+anamarijakukulj@users.noreply.github.com>
2023-02-27 09:48:03 +0100

    Booklist

--- LearnReact/my-app/src/index.js
+++ LearnReact/my-app/src/index.js
@@ -1,9 +1,51 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-function Greetings() {
-  return <h2>My First Component</h2>;
-}
+// function Greetings() {
+//   return (
+//     <>
+//       <Person />
+//       <Message />
+//     </>
+//   );
+// }
+
+// const Person = () => <h2>john doe</h2>;
+// const Message = () => {
+//   return <p>this is my message</p>;
+// };
+
+// function Greetings() {
+//   return React.createElement("h2", {}, "hello world");
+// }
+
+const BookList = () => {
+  return (
+    <section>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+};
+
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => <h2>image placeholder</h2>;
+const Title = () => <h2>Book Title</h2>;
+const Author = () => {
+  return <h4>Author</h4>;
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Greetings />);
+root.render(<BookList />);
