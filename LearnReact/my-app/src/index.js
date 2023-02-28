cd86f6c936738274ffd749ae291cdcb06db2580e
Anamarija <97092170+anamarijakukulj@users.noreply.github.com>
2023-02-28 15:07:45 +0100

    JSX - JS

--- LearnReact/my-app/src/index.js
+++ LearnReact/my-app/src/index.js
@@ -33,30 +33,19 @@ const BookList = () => {
   );
 };
 
+const author = "James Clear";
 const Book = () => {
+  const title = "Atomic Habits";
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img
+        src="https://m.media-amazon.com/images/I/51B7kuFwQFL._SY344_BO1,204,203,200_.jpg"
+        alt="Atomic Habits"
+      />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
     </article>
   );
-};
-
-const Image = () => (
-  <img
-    src="https://m.media-amazon.com/images/I/51B7kuFwQFL._SY344_BO1,204,203,200_.jpg"
-    alt="Atomic Habits"
-  />
-);
-const Title = () => <h2>Atomic Habits</h2>;
-const Author = () => {
-  const inlineHeadingStyles = {
-    color: "#617d98",
-    fontSize: "0.75rem",
-    marginTop: "0.5rem",
-  };
-  return <h4 style={inlineHeadingStyles}>James Clear</h4>;
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
