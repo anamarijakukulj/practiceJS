611e4749d584370d24a705dfa69f15ca77b5c038
Anamarija <97092170+anamarijakukulj@users.noreply.github.com>
2023-03-01 10:51:20 +0100

    Add list, work with props

--- LearnReact/my-app/src/index.js
+++ LearnReact/my-app/src/index.js
@@ -3,49 +3,38 @@ import ReactDOM from "react-dom/client";
 
 import "./index.css";
 
-const firstBook = {
-  author: "James Clear",
-  title: "Atomic Habits",
-  img: "https://m.media-amazon.com/images/I/51B7kuFwQFL._SY344_BO1,204,203,200_.jpg",
-};
-
-const secondBook = {
-  author: "Alex North ",
-  title: "The Half Burnt House:",
-  img: "https://m.media-amazon.com/images/I/51GCvvcZeOL._SY264_BO1,204,203,200_QL40_ML2_.jpg",
-};
+const books = [
+  {
+    author: "James Clear",
+    title: "Atomic Habits",
+    img: "https://m.media-amazon.com/images/I/51B7kuFwQFL._SY344_BO1,204,203,200_.jpg",
+    id: 1,
+  },
+  {
+    author: "Alex North ",
+    title: "The Half Burnt House:",
+    img: "https://m.media-amazon.com/images/I/51GCvvcZeOL._SY264_BO1,204,203,200_QL40_ML2_.jpg",
+    id: 2,
+  },
+];
 
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book
-        author={firstBook.author}
-        title={firstBook.title}
-        img={firstBook.img}
-      >
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt
-          nostrum adipisci modi eaque sunt officiis, reiciendis fuga maxime eum
-          asperiores.
-        </p>
-        <button>Click me</button>
-      </Book>
-      <Book
-        author={secondBook.author}
-        title={secondBook.title}
-        img={secondBook.img}
-      ></Book>
+      {books.map((book) => {
+        return <Book {...book} key={book.id} />;
+      })}
     </section>
   );
 };
 
-const Book = ({ img, title, author, children }) => {
+const Book = (props) => {
+  const { img, title, author } = props;
   return (
     <article className="book">
       <img src={img} alt={title} />
       <h2>{title}</h2>
       <h4>{author}</h4>
-      {children}
     </article>
   );
 };
