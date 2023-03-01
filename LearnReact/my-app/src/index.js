b103e9170cf1d6c7379ea72badd06dd549bb8a81
Anamarija <97092170+anamarijakukulj@users.noreply.github.com>
2023-03-01 14:02:03 +0100

    Delete event listeners

--- LearnReact/my-app/src/index.js
+++ LearnReact/my-app/src/index.js
@@ -21,48 +21,9 @@ const books = [
 const BookList = () => {
   return (
     <section className="booklist">
-      <EventExample />
       {books.map((book) => {
         return <Book {...book} key={book.id} />;
       })}
-    </section>
-  );
-};
-
-const EventExample = () => {
-  const handleFormInput = (e) => {
-    console.log(e.target);
-    console.log(e.target.name);
-    console.log(e.target.value);
-    console.log("handle form input");
-  };
-
-  const handleButtonClick = () => {
-    alert("handle button click");
-  };
-
-  const handleFormSubmit = (e) => {
-    e.preventDefault();
-    console.log("Form submitted");
-  };
-
-  return (
-    <section>
-      <form>
-        <h2>Typical Form</h2>
-        <input
-          type="text"
-          name="example"
-          onChange={handleFormInput}
-          style={{ margin: "1rem 0" }}
-        ></input>
-      </form>
-      <button type="submit" onClick={handleFormSubmit}>
-        submit
-      </button>
-      <div>
-        <button onClick={handleButtonClick}>click me</button>
-      </div>
     </section>
   );
 };
