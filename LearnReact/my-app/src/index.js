91d4edf5addfaf292ad31b268fa58546e989e413
Anamarija <97092170+anamarijakukulj@users.noreply.github.com>
2023-03-01 10:06:50 +0100

    Props children

--- LearnReact/my-app/src/index.js
+++ LearnReact/my-app/src/index.js
@@ -2,28 +2,50 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "./index.css";
-const author = "James Clear";
-const title = "Atomic Habits";
-// const image = "./Images/book.jpg";
-const image =
-  "https://m.media-amazon.com/images/I/51B7kuFwQFL._SY344_BO1,204,203,200_.jpg";
+
+const firstBook = {
+  author: "James Clear",
+  title: "Atomic Habits",
+  img: "https://m.media-amazon.com/images/I/51B7kuFwQFL._SY344_BO1,204,203,200_.jpg",
+};
+
+const secondBook = {
+  author: "Alex North ",
+  title: "The Half Burnt House:",
+  img: "https://m.media-amazon.com/images/I/51GCvvcZeOL._SY264_BO1,204,203,200_QL40_ML2_.jpg",
+};
 
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book author={author} title={title} img={image} />
-      <Book author={author} title={title} img={image} />
+      <Book
+        author={firstBook.author}
+        title={firstBook.title}
+        img={firstBook.img}
+      >
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt
+          nostrum adipisci modi eaque sunt officiis, reiciendis fuga maxime eum
+          asperiores.
+        </p>
+        <button>Click me</button>
+      </Book>
+      <Book
+        author={secondBook.author}
+        title={secondBook.title}
+        img={secondBook.img}
+      ></Book>
     </section>
   );
 };
 
-const Book = (props) => {
-  console.log(props);
+const Book = ({ img, title, author, children }) => {
   return (
     <article className="book">
-      <img src={props.img} alt={props.title} />
-      <h2>{props.title}</h2>
-      <h4>{props.author}</h4>
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
+      {children}
     </article>
   );
 };
