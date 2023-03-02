45b74e7c0e91caeda273e620b394e57255d8318b
Anamarija <97092170+anamarijakukulj@users.noreply.github.com>
2023-03-02 10:26:58 +0100

    Remove Book and booklist

--- LearnReact/my-app/src/index.js
+++ LearnReact/my-app/src/index.js
@@ -2,41 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "./index.css";
-
-const books = [
-  {
-    author: "James Clear",
-    title: "Atomic Habits",
-    img: "https://m.media-amazon.com/images/I/51B7kuFwQFL._SY344_BO1,204,203,200_.jpg",
-    id: 1,
-  },
-  {
-    author: "Alex North ",
-    title: "The Half Burnt House:",
-    img: "https://m.media-amazon.com/images/I/51GCvvcZeOL._SY264_BO1,204,203,200_QL40_ML2_.jpg",
-    id: 2,
-  },
-];
+import { books } from "./books";
+import Book from "./Book";
 
 const BookList = () => {
   return (
-    <section className="booklist">
-      {books.map((book) => {
-        return <Book {...book} key={book.id} />;
-      })}
-    </section>
-  );
-};
-
-const Book = (props) => {
-  const { img, title, author } = props;
-
-  return (
-    <article className="book">
-      <img src={img} alt={title} />
-      <h2>{title}</h2>
-      <h4>{author}</h4>
-    </article>
+    <>
+      <h1>amazon best sellers</h1>
+      <section className="booklist">
+        {books.map((book, index) => {
+          return <Book {...book} key={book.id} number={index} />;
+        })}
+      </section>
+    </>
   );
 };
 
