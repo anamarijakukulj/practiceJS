149710a84d260575ecae6f39afead3bdf47d208d
Anamarija <97092170+anamarijakukulj@users.noreply.github.com>
2023-03-02 10:26:08 +0100

    Add book list

--- LearnReact/my-app/src/books.js
+++ LearnReact/my-app/src/books.js
@@ -1,1 +1,25 @@
+import img1 from "./images/book.jpg";
+import img2 from "./images/book-2.jpeg";
+import img3 from "./images/book-3.jpg";
+
+export const books = [
+  {
+    author: "James Clear",
+    title: "Atomic Habits",
+    img: img1,
+    id: 1,
+  },
+  {
+    author: "Alex North",
+    title: "The Half Burnt House",
+    img: img2,
+    id: 2,
+  },
+  {
+    author: "Charlie Mackesy",
+    title: "The Boy, the Mole, the Fox and the Horse",
+    img: img3,
+    id: 3,
+  },
+];
 
