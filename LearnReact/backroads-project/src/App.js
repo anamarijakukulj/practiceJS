644c792a06923fd323d69cfa84443da3fff38e35
Anamarija <97092170+anamarijakukulj@users.noreply.github.com>
2023-03-07 12:27:27 +0100

    Update App.js

--- LearnReact/backroads-project/src/App.js
+++ LearnReact/backroads-project/src/App.js
@@ -1,28 +1,6 @@
 function App() {
   return (
     <>
-    <!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>Backroads || Tour Company</title>
-    <!-- favicon -->
-    <link rel="shortcut icon" href="./images/favicon.ico" type="image/x-icon" />
-    <!-- font-awesome -->
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0/css/all.min.css"
-      integrity="sha512-c93ifPoTvMdEJH/rKIcBx//AL1znq9+4/RmMGafI/vnTFe/dKwnn1uoeszE2zJBQTS1Ck5CqSBE+34ng2PthJg=="
-      crossorigin="anonymous"
-      referrerpolicy="no-referrer"
-    />
-
-    <!-- styles css -->
-    <link rel="stylesheet" href="./css/styles.css" />
-  </head>
-  <body>
     <nav class="navbar">
       <div class="nav-center">
         <div class="nav-header">
@@ -286,9 +264,6 @@ function App() {
         <span id="date"></span> all rights reserved
       </p>
     </footer>
-    <script src="./js/app.js"></script>
-  </body>
-</html>
     </>
   );
 }
