14a6907502128188c74565830c01c0564d986cb0
anamarija.kukulj <anamarija.kukulj@ibmix.hr>
2023-03-08 11:18:21 +0100

    Finish useState Gotcha

--- LearnReact/advance-react-app/src/projects/01-useState/05-useState-gotcha.jsx
+++ LearnReact/advance-react-app/src/projects/01-useState/05-useState-gotcha.jsx
@@ -1,5 +1,30 @@
+import { useState } from "react";
+
 const UseStateGotcha = () => {
-  return <h2>useState "gotcha"</h2>;
+  const [value, setValue] = useState(0);
+
+  const handleClick = () => {
+    // setValue((currentState) => {
+    //   const newState = currentState + 1;
+    //   return newState;
+    // });
+
+    setTimeout(() => {
+      console.log("clicked button");
+      setValue((currentState) => {
+        return currentState + 1;
+      });
+    }, 3000);
+  };
+
+  return (
+    <>
+      <h3>{value}</h3>
+      <button type="button" className="btn" onClick={handleClick}>
+        increase
+      </button>
+    </>
+  );
 };
 
 export default UseStateGotcha;
