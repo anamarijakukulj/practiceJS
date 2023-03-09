585a28bd91798391509e629da1b3546b70d533f4
Anamarija <97092170+anamarijakukulj@users.noreply.github.com>
2023-03-09 15:52:51 +0100

    Finish simple birthday reminder

--- LearnReact/birthday-reminder-app/src/App.jsx
+++ LearnReact/birthday-reminder-app/src/App.jsx
@@ -3,7 +3,17 @@ import data from "./data";
 import List from "./List";
 
 function App() {
-  return <p>Birthday App 2</p>;
+  const [people, setPeople] = useState(data);
+
+  return (
+    <main>
+      <section className="container">
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        <button onClick={() => setPeople([])}>clear all</button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
