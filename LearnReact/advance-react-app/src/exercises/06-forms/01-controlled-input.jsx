9390a4049a125ad7032596d6347d7fb32fba9885
Anamarija <97092170+anamarijakukulj@users.noreply.github.com>
2023-03-15 09:45:35 +0100

    Add useState

--- LearnReact/advance-react-app/src/exercises/06-forms/01-controlled-input.jsx
+++ LearnReact/advance-react-app/src/exercises/06-forms/01-controlled-input.jsx
@@ -1,4 +1,15 @@
+import { useState } from 'react';
+
 const ControlledInputs = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //do something
+    console.log(name, email);
+  };
+  
   return (
     <form className="form">
       <h4>controlled inputs</h4>
@@ -7,14 +18,14 @@ const ControlledInputs = () => {
         <label htmlFor="name" className="form-label">
           name
         </label>
-        <input type="text" id="name" className="form-input" />
+        <input type="text" id="name" className="form-input" value={name} onChange={(e) => setName(e.target.value)} />
       </div>
     
       <div className="form-row">
         <label htmlFor="email" className="form-label">
           email
         </label>
-        <input type="email" id="email" className="form-input" />
+        <input type="email" id="email" className="form-input" value={email} onChange={(e) => setEmail(e.target.value)}  />
       </div>
     
       <button type="submit" className="btn btn-block">
