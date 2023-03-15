149a61ad956166e806a18c244115dba0a6948f31
Anamarija <97092170+anamarijakukulj@users.noreply.github.com>
2023-03-15 09:24:31 +0100

    Update Person's image

--- LearnReact/advance-react-app/src/exercises/05-leverage-javascript/Person.jsx
+++ LearnReact/advance-react-app/src/exercises/05-leverage-javascript/Person.jsx
@@ -1,5 +1,9 @@
+import avatar from "../../assets/default-avatar.svg";
+
 export function Person({name, nickName = "shakeAndBake", images}) {
-  const img = images && images[0] && images[0].small && images[0].small.url;
+//   const img = images && images[0] && images[0].small && images[0].small.url;
+//   const img = images?.[0]?.small?.url || avatar;
+  const img = images?.[0]?.small?.ulr ?? avatar;
   
   return (
     <div>
