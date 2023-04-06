f0879383b0838eb2b4e161c1d1a904a71f6b8212
Anamarija <97092170+anamarijakukulj@users.noreply.github.com>
2023-04-06 11:11:37 +0200

    Import getContacts

--- LearnReact/Tutorial/React Router/router-tutorial/src/routes/root.jsx
+++ LearnReact/Tutorial/React Router/router-tutorial/src/routes/root.jsx
@@ -1,4 +1,10 @@
 import { Outlet, Link } from "react-router-dom";
+import { geContacts } from "../contact";
+
+export async function loader() {
+  const contacts = await getContacts();
+  return { contacts };
+}
 
 export default function Root() {
   return (
