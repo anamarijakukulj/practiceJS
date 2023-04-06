34b16be525eaa95cca347504d5abb5ec1dd99f09
Anamarija <97092170+anamarijakukulj@users.noreply.github.com>
2023-04-06 14:02:28 +0200

    Update root.jsx

--- LearnReact/Tutorial/React Router/router-tutorial/src/routes/root.jsx
+++ LearnReact/Tutorial/React Router/router-tutorial/src/routes/root.jsx
@@ -1,5 +1,10 @@
-import { Outlet, Link, userLoaderData } from "react-router-dom";
-import { geContacts } from "../contact";
+import { Outlet, Link, useLoaderData, Form, } from "react-router-dom";
+import { getContacts, createContact } from "../contacts";
+
+export async function action() {
+  const contact = await createContact();
+  return {contact};
+}
 
 export async function loader() {
   const contacts = await getContacts();
@@ -7,6 +12,8 @@ export async function loader() {
 }
 
 export default function Root() {
+  const { contacts } = useLoaderData();
+
   return (
     <>
       <div id="sidebar">
@@ -30,9 +37,9 @@ export default function Root() {
               aria-live="polite"
             ></div>
           </form>
-          <form method="post">
+          <Form method="post">
             <button type="submit">New</button>
-          </form>
+          </Form>
         </div>
         <nav> {contacts.length ? (
             <ul>
