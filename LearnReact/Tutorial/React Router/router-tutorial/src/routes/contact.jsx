4f2bda0ec54b5ab17bbbf5058a5eb41d29b21bb5
Anamarija <97092170+anamarijakukulj@users.noreply.github.com>
2023-04-07 10:31:29 +0200

    Add favorite label

--- LearnReact/Tutorial/React Router/router-tutorial/src/routes/contact.jsx
+++ LearnReact/Tutorial/React Router/router-tutorial/src/routes/contact.jsx
@@ -1,8 +1,21 @@
-import { Form, useLoaderData } from "react-router-dom";
-import { getContact } from "../contacts";
+import { Form, useLoaderData, useFetcher } from "react-router-dom";
+import { getContact, updateContact } from "../contacts";
+
+export async function action({request, params}) {
+  let formData = await request.formData();
+  return updateContact(params.contactId, {
+    favorite: formData.get("favorite") === "true"
+  })
+}
 
 export async function loader({params}) {
   const contact = await getContact(params.contactId);
+  if(!contact) {
+    throw new Response("", {
+      status: 404,
+      statusText: "Not Found"
+    });
+  }
   return {contact};
 }
 
@@ -78,9 +91,11 @@ export default function Contact() {
 }
 
 function Favorite({ contact }) {
+  const fetcher = useFetcher();
   let favorite = contact.favorite;
+
   return (
-    <Form method="post">
+    <fetcher.Form method="post">
       <button
         name="favorite"
         value={favorite ? "false" : "true"}
@@ -92,6 +107,6 @@ function Favorite({ contact }) {
       >
         {favorite ? "★" : "☆"}
       </button>
-    </Form>
+    </fetcher.Form>
   );
 }
