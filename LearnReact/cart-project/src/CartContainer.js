68b11f87c2d92c4746ba9d87091831a90d73e3a6
Anamarija <97092170+anamarijakukulj@users.noreply.github.com>
2023-04-24 12:44:59 +0200

    Update CartContainer.js

--- LearnReact/cart-project/src/CartContainer.js
+++ LearnReact/cart-project/src/CartContainer.js
@@ -19,24 +19,29 @@ const CartContainer = () => {
 
   return (
     <section className='cart'>
+    
       {/* cart header */}
       <header>
         <h2>your bag</h2>
       </header>
+
       {/* cart items */}
       <div>
         {cart.map((item) => {
           return <CartItem key={item.id} {...item} />
         })}
       </div>
+
       {/* cart footer */}
       <footer>
         <hr />
+        
         <div className='cart-total'>
           <h4>
             total <span>$0.00</span>
           </h4>
         </div>
+
         <button
           className='btn clear-btn'
           onClick={() => console.log('clear cart')}
