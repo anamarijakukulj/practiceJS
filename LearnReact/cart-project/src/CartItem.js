984adde33d5c1eafc185969fe8af1a03af84979b
Anamarija <97092170+anamarijakukulj@users.noreply.github.com>
2023-04-24 13:13:26 +0200

    Add increase and decrease buttons

--- LearnReact/cart-project/src/CartItem.js
+++ LearnReact/cart-project/src/CartItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaChevronUp, FaChevronDown } from 'react-icons/fa';
 import { useGlobalContext } from './context';
 
 const CartItem = ({ id, img, title, price, amount }) => {
@@ -23,9 +24,7 @@ const CartItem = ({ id, img, title, price, amount }) => {
       <div>
         {/* increase amount */}
         <button className='amount-btn' onClick={() => console.log('increase')}>
-          <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20'>
-            <path d='M10.707 7.05L10 6.343 4.343 12l1.414 1.414L10 9.172l4.243 4.242L15.657 12z' />
-          </svg>
+          <FaChevronUp />
         </button>
 
         {/* amount */}
@@ -33,9 +32,7 @@ const CartItem = ({ id, img, title, price, amount }) => {
 
         {/* decrease amount */}
         <button className='amount-btn' onClick={() => console.log('decrease')}>
-          <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20'>
-            <path d='M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z' />
-          </svg>
+          <FaChevronDown />
         </button>
       </div>
     </article>
