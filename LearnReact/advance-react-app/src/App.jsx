11750fe765886ea2ee3ba86705fbf9ebb62d277c
Anamarija <97092170+anamarijakukulj@users.noreply.github.com>
2023-04-28 12:34:21 +0200

    Update App.jsx

--- LearnReact/advance-react-app/src/App.jsx
+++ LearnReact/advance-react-app/src/App.jsx
@@ -1,9 +1,9 @@
-import LowerState from "./exercises/11-performance/03-hooks";
+import LatestReact from "./exercises/11-performance/04-react-18";
 
 function App() {
   return (
     <div className="container">
-      <LowerState />
+      <LatestReact />
     </div>
   );
 }
