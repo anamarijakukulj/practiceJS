500dc9e5b63c4e7dd27428b0d07946562c2589b2
Anamarija <97092170+anamarijakukulj@users.noreply.github.com>
2023-04-28 12:13:14 +0200

    Add useTransition

--- LearnReact/advance-react-app/src/exercises/11-performance/04-react-18/index.jsx
+++ LearnReact/advance-react-app/src/exercises/11-performance/04-react-18/index.jsx
@@ -1,21 +1,27 @@
-import { useState } from 'react';
+import { useState, useTransition } from 'react';
+
 const LatestReact = () => {
   const [text, setText] = useState('');
   const [items, setItems] = useState([]);
+  const [isPendingm startTransition] = useTransition()
 
   const handleChange = (e) => {
     setText(e.target.value);
 
     // slow down CPU
-    // const newItems = Array.from({ length: 5000 }, (_, index) => {
-    //   return (
-    //     <div key={index}>
-    //       <img src='/vite.svg' alt='' />
-    //     </div>
-    //   );
-    // });
-    // setItems(newItems);
+    startTransition(() => {
+      const newItems = Array.from({ length: 5000 }, (_, index) => {
+        return (
+          <div key={index}>
+            <img src='/vite.svg' alt='' />
+          </div>
+        );
+      });
+      setItems(newItems);
+    });
   };
+    
+
   return (
     <section>
       <form className='form'>
@@ -26,17 +32,23 @@ const LatestReact = () => {
           onChange={handleChange}
         />
       </form>
+
       <h4>Items Below</h4>
 
-      <div
+      {isPending ? (
+        <h4>Loading...</h4>
+      ) : (
+        <div
         style={{
           display: 'grid',
           gridTemplateColumns: '1fr 1fr 1fr',
           marginTop: '2rem',
         }}
-      >
+        >
         {items}
-      </div>
+        </div> 
+      )}
+
     </section>
   );
 };
