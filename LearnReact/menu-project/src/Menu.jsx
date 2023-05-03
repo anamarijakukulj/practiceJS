f8487202b4a76d432293db66e28997549f4f35b5
Anamarija <97092170+anamarijakukulj@users.noreply.github.com>
2023-05-03 13:58:14 +0200

    Add the structure

--- LearnReact/menu-project/src/Menu.jsx
+++ LearnReact/menu-project/src/Menu.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import MenuItem from './MenuItem';
 
-const Menu = () => {
-  return <h2>menu component</h2>;
+const Menu = ({items}) => {
+  return (
+    <div className='section-center'>
+      {items.map((item) => {
+        return(
+          <MenuItem key={item.id} {...item} />
+        )
+      })}
+    </div>
+  );
 };
 
 export default Menu;
