8a029a3abefec461ea892a3cee1d22f39c49e443
Anamarija <97092170+anamarijakukulj@users.noreply.github.com>
2023-05-05 09:03:19 +0200

    Fetch job

--- LearnReact/tabs-project/src/App.js
+++ LearnReact/tabs-project/src/App.js
@@ -1,10 +1,40 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { FaAngleDoubleRight } from 'react-icons/fa';
+import JobInfo from './JobInfo';
 
 const url = 'https://course-api.com/react-tabs-project';
 
 function App() {
-  return <h2>tabs project setup</h2>
+  const[isLoading, setIsLoading] = useState(true);
+  const[jobs, setJobs] = useState([]);
+  //currentItem
+
+  const fetchJobs = async() => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+    setJobs(newJobs);
+    setIsLoading(false);
+  }
+
+  useEffect(() => {
+    fetchJobs();
+  }, [])
+  
+  if(isLoading) {
+    return(
+      <section className='jobs-center'>
+        <div className='loading'></div>
+      </section>
+    )
+  };
+
+  return (
+    <section className='jobs-center'>
+      {/*button container */}
+      {/*job info */}
+      <JobInfo jobs={jobs} />
+    </section>
+  );
 };
 
 export default App;
