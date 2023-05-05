a533ce561caed372a4126212cdf00395698f818c
anamarija.kukulj <anamarija.kukulj@ibmix.hr>
2023-05-05 11:26:09 +0200

    Add funcionality to prev i next buttons

--- LearnReact/slider-project/src/Carousel.js
+++ LearnReact/slider-project/src/Carousel.js
@@ -1,19 +1,37 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import list from './data';
 import { FaQuoteRight, FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 
 const Carousel =() => {
     const[people,setPeople] = useState(list);
+    const [currentPerson, setCurrentPerson] = useState(0);
 
-    const prevSlide = () => {};
-    const nextSlide = () => {};
+    const prevSlide = () => {
+        setCurrentPerson((oldPerson) => {
+            const result = (oldPerson - 1 + people.length) % people.length;
+            return result;
+        })
+    };
+
+    const nextSlide = () => {
+        setCurrentPerson((oldPerson) => {
+            const result = (oldPerson + 1) % people.length;
+            return result;
+        })
+    };
 
     return (
         <section className='slider-container'>
-            {people.map((person) => {
+            {people.map((person, personIndex) => {
                 const {id, image, name, title, quote} = person;
                 return (
-                    <article className='slide' key={id}>
+                    <article 
+                        className='slide' 
+                        style={{transform:`translateX(${100 * (personIndex - currentPerson)}%)`, 
+                        opacity: personIndex === currentPerson ? 1 : 0,
+                        visibility: personIndex === currentPerson ? "visible" : "hidden"}} 
+                        key={id}
+                    >
                         <img src={image} alt={name} className='person-img' />
                         <h4 className='name'>{name}</h4>
                         <p className='title'>{title}</p>
