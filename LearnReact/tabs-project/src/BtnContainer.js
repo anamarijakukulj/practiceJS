c7a8a5285c5e7364e6c6e15123d2180db4dde9bf
Anamarija <97092170+anamarijakukulj@users.noreply.github.com>
2023-05-05 09:28:31 +0200

    Create BtnContainer

---
+++ LearnReact/tabs-project/src/BtnContainer.js
@@ -0,0 +1,19 @@
+const BtnContainer = ({jobs, currentItem, setCurrentItem}) => {
+    return (
+        <div className="btn-container">
+            {jobs.map((job, index) => {
+                return (
+                    <button 
+                        key={job.id} 
+                        onClick={() => setCurrentItem(index)}
+                        className={index === currentItem ? 'active-btn' : 'job-btn'}
+                    >
+                        {job.company}
+                    </button>
+                )
+            })}
+        </div>
+    );
+};
+
+export default BtnContainer;
