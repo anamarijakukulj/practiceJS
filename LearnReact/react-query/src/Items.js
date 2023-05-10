7d4da35af706a032881be777213f2b0b47e74957
Anamarija <97092170+anamarijakukulj@users.noreply.github.com>
2023-05-10 07:44:02 +0200

    Add Query

--- LearnReact/react-query/src/Items.js
+++ LearnReact/react-query/src/Items.js
@@ -1,8 +1,32 @@
+import {useQuery} from '@tanstack/react-query';
 import SingleItem from './SingleItem';
-const Items = ({ items }) => {
+import customFetch from './utils';
+
+const Items = () => {
+  const {isLoading, data, isError, error} = useQuery({
+    queryKey: ['tasks'],
+    queryFn: async() => {
+      const {data} = await customFetch.get('/');
+      return data;
+    },
+  });
+  
+  if(isLoading) {
+    return <p style={{marginTop: '1rem'}}>Loading...</p>
+  }
+
+  // if(isError) {
+  //   return <p style={{marginTop: '1rem'}}>There was an error...</p>
+  // }
+
+  console.log(error);
+  if(error) {
+    return <p style={{marginTop: '1rem'}}>{error.response.data}</p>
+  }
+
   return (
     <div className='items'>
-      {items.map((item) => {
+      {data.taskList.map((item) => {
         return <SingleItem key={item.id} item={item} />;
       })}
     </div>
