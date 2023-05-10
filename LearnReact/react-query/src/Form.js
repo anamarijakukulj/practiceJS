2d92604f06f1e9e9dd7e499d4af2d639b9b180f9
Anamarija <97092170+anamarijakukulj@users.noreply.github.com>
2023-05-10 10:12:01 +0200

    Add createTasks

--- LearnReact/react-query/src/Form.js
+++ LearnReact/react-query/src/Form.js
@@ -1,10 +1,21 @@
+import { useQueryClient, useMutation } from '@tanstack/react-query';
 import { useState } from 'react';
+import customFetch from './utils';
+import { toast } from 'react-toastify';
+import { useCreateTask } from './reactQueryCustomHooks';
 
 const Form = () => {
   const [newItemName, setNewItemName] = useState('');
 
+  const {isLoading, createTask} = useCreateTask();
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    createTask(newItemName, {
+      onSuccess: () => {
+        setNewItemName('');
+      }
+    });
   };
   return (
     <form onSubmit={handleSubmit}>
@@ -16,7 +27,7 @@ const Form = () => {
           value={newItemName}
           onChange={(event) => setNewItemName(event.target.value)}
         />
-        <button type='submit' className='btn'>
+        <button type='submit' className='btn' disabled={isLoading}>
           add task
         </button>
       </div>
