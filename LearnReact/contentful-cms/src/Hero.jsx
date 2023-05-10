d5fc9fc1861f36743f2059edba3141b1ee9d15b3
Anamarija <97092170+anamarijakukulj@users.noreply.github.com>
2023-05-10 14:21:44 +0200

    Create Hero.jsx

---
+++ LearnReact/contentful-cms/src/Hero.jsx
@@ -0,0 +1,22 @@
+const Hero = () => {
+    return (
+        <section className="hero">
+            <div className="hero-center">
+                <div className="hero-title">
+                    <h1>Contentful CMS</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum accusantium quos quis? Deserunt nostrum provident alias temporibus, 
+                        magni nemo autem? Natus error beatae eveniet ratione rem impedit ullam minima debitis, quaerat itaque accusantium,
+                        dolorem nam quo assumenda iste. Quia sequi libero quaerat porro eveniet autem, fuga saepe molestiae alias odit.               
+                    </p>
+                </div>
+
+                <div className="img-container">
+                    <img src="" alt="" />
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Hero;
