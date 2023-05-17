4021ceeeae0128fb3d23c3b2a7fe367a795d7beb
Anamarija <97092170+anamarijakukulj@users.noreply.github.com>
2023-05-17 10:40:29 +0200

    Add routes

--- LearnReact/react-router-tutorial/src/App.js
+++ LearnReact/react-router-tutorial/src/App.js
@@ -1,16 +1,39 @@
+import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Products from './pages/Products';
+import SingleProduct from './pages/SingleProduct';
 import Error from './pages/Error';
+import SharedLayout from './pages/SharedLayout';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import ProtectedRoute from './pages/ProtectedRoute';
+import SharedProductLayout from './pages/SharedProductLayout';
  
 function App() {
+  const [user, setUser] = useState(null);
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />}>
+        <Route path='/' element={<SharedLayout />}>
+          <Route index element={<Home />} />
           <Route path='about' element={<About />} />
-          <Route path='products' element={<Products />} />
+
+          <Route path='products' element={<SharedProductLayout />}>
+            <Route index element={<Products />} />
+            <Route path=':productId' element={<SingleProduct />} />
+          </Route>
+          
+          <Route path='login' element={<Login setUser={setUser}></Login>} />
+          <Route 
+            path='dashboard' 
+            element={
+              <ProtectedRoute user={user}>
+                <Dashboard user={user}></Dashboard>
+              </ProtectedRoute>} 
+          />
           <Route path='*' element={<Error />} />
         </Route>
       </Routes>
