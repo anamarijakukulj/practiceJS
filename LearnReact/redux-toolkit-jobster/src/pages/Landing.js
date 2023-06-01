9135676ba4c0336c388d89e48607ff563e225730
Anamarija <97092170+anamarijakukulj@users.noreply.github.com>
2023-06-01 10:35:55 +0200

    Add Link

--- LearnReact/redux-toolkit-jobster/src/pages/Landing.js
+++ LearnReact/redux-toolkit-jobster/src/pages/Landing.js
@@ -1,6 +1,7 @@
 import main from "../assets/images/main.svg";
 import { Logo } from "../components";
 import Wrapper from "../assets/wrappers/LandingWrap.js";
+import { Link } from "react-router-dom";
 
 const LandingPage = () => {
   return (
@@ -23,7 +24,9 @@ const LandingPage = () => {
             ullamcorper. Mauris.
           </p>
 
-          <button className="btn btn-hero">Login/Register</button>
+          <Link to="/register" className="btn btn-hero">
+            Login/Register
+          </Link>
         </div>
 
         <img src={main} alt="job hunt" className="img main-img" />
