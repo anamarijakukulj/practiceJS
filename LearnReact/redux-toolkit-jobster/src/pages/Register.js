31aefc4681324a070e87b9e7e34196e506d81938
Anamarija <97092170+anamarijakukulj@users.noreply.github.com>
2023-06-01 10:36:25 +0200

    Add structure

--- LearnReact/redux-toolkit-jobster/src/pages/Register.js
+++ LearnReact/redux-toolkit-jobster/src/pages/Register.js
@@ -1,5 +1,80 @@
+import { useState, useEffect } from "react";
+import Logo from "../components/Logo";
+import Wrapper from "../assets/wrappers/RegisterWrap";
+
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  isMember: true,
+};
+
 const RegisterPage = () => {
-  return <h4>Register page</h4>;
+  const [values, setValues] = useState(initialState);
+
+  const handleChange = (e) => {
+    console.log(e.target);
+  };
+
+  const onSubmit = (e) => {
+    e.prebentDefault();
+    console.log(e.target);
+  };
+
+  return (
+    <Wrapper className="full-page">
+      <form className="form" onSubmit={onSubmit}>
+        <Logo />
+        <h3>Login</h3>
+
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
+            name
+          </label>
+
+          <input
+            type="text"
+            name="name"
+            value={values.name}
+            onChange={handleChange}
+            className="form-input"
+          />
+        </div>
+
+        <div className="form-row">
+          <label htmlFor="email" className="form-label">
+            email
+          </label>
+
+          <input
+            type="email"
+            name="email"
+            value={values.email}
+            onChange={handleChange}
+            className="form-input"
+          />
+        </div>
+
+        <div className="form-row">
+          <label htmlFor="password" className="form-label">
+            password
+          </label>
+
+          <input
+            type="password"
+            name="password"
+            value={values.password}
+            onChange={handleChange}
+            className="form-input"
+          />
+        </div>
+
+        <button type="submit" className="btn btn-block">
+          submit
+        </button>
+      </form>
+    </Wrapper>
+  );
 };
 
 export default RegisterPage;
