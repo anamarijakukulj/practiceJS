8e84c6cb24551bbd7d9a8a707462de1686fa7b90
Anamarija <97092170+anamarijakukulj@users.noreply.github.com>
2023-06-01 10:35:29 +0200

    Edit error page

--- LearnReact/redux-toolkit-jobster/src/pages/Error.js
+++ LearnReact/redux-toolkit-jobster/src/pages/Error.js
@@ -1,5 +1,21 @@
+import { Link } from "react-router-dom";
+import image from "../assets/images/not-found.svg";
+import Wrapper from "../assets/wrappers/ErrorWrap";
+
 const ErrorPage = () => {
-  return <h4>Error page</h4>;
+  return (
+    <Wrapper classname="full-page">
+      <div>
+        <img src={image} alt="not found" />
+
+        <h3>Ohh, Page NotFound</h3>
+
+        <p>We can't seem to find the page you're looking for</p>
+
+        <Link to="/">back home</Link>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default ErrorPage;
