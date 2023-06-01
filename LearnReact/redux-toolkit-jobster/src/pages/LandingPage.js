83aa39899286e99e7f41f5aee9c99f3a25b182e2
Anamarija <97092170+anamarijakukulj@users.noreply.github.com>
2023-06-01 09:12:10 +0200

    Add structure and styling

--- LearnReact/redux-toolkit-jobster/src/pages/LandingPage.js
+++ LearnReact/redux-toolkit-jobster/src/pages/LandingPage.js
@@ -1,7 +1,79 @@
+import logo from "../assets/images/logo.svg";
+import main from "../assets/images/main.svg";
+import styled from "styled-components";
+
 const LandingPage = () => {
   return (
-    <div>Landing page</div>
-  )
+    <Wrapper>
+      <nav>
+        <img src={logo} alt="jobster logo" className="logo" />
+      </nav>
+
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in
+            fringilla nibh. Vivamus lobortis auctor ultrices. In a nibh
+            tristique, tristique arcu ac, aliquet risus. Morbi eget lacus ut leo
+            elementum convallis. Morbi consectetur sit amet nulla vel
+            ullamcorper. Mauris.
+          </p>
+
+          <button className="btn btn-hero">Login/Register</button>
+        </div>
+
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.main`
+  nav {
+    width: var(--fluid-width);
+    max-width: var(--max-width);
+    margin: 0 auto;
+    height: var(--nav-height);
+    display: flex;
+    align-items: center;
+  }
+
+  .page {
+    min-height: calc(100vh - var(--nav-height));
+    display: grid;
+    align-items: center;
+    margin-top: 3rem;
+  }
+
+  h1 {
+    font-weight: 700;
+    span {
+      color: var(--primary-500);
+    }
+  }
+
+  p {
+    color: var(--grey-600);
+  }
+
+  .main-img {
+    display: none;
+  }
+
+  @media screen and (min-width: 992px) {
+    .page {
+      grid-template-columns: 1fr 1fr;
+      column-gap: 3rem;
+    }
+
+    .main-img {
+      display: block;
+    }
+  }
+`;
 
 export default LandingPage;
