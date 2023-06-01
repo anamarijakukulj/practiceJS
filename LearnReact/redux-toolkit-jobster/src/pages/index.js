5bd5a32c2e457ad70e9f12af233f448cd1c7d213
Anamarija <97092170+anamarijakukulj@users.noreply.github.com>
2023-06-01 09:37:36 +0200

    Import and export pages

---
+++ LearnReact/redux-toolkit-jobster/src/pages/index.js
@@ -0,0 +1,6 @@
+import LandingPage from "./LandingPage";
+import ErrorPage from "./ErrorPage";
+import RegisterPage from "./RegisterPage";
+import DashboardPage from "./DashboardPage";
+
+export { LandingPage, ErrorPage, RegisterPage, DashboardPage };
