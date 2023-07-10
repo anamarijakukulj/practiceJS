42647cb3cff1c437d31f2294ad842c43ad33cfae
Anamarija <97092170+anamarijakukulj@users.noreply.github.com>
2023-07-10 09:59:19 +0200

    Add logout

--- LearnReact/redux-toolkit-jobster/src/components/Navbar.js
+++ LearnReact/redux-toolkit-jobster/src/components/Navbar.js
@@ -3,9 +3,10 @@ import {FaAlignLeft, FaUserCircle, FaCaretDown} from "react-icons/fa";
 import Logo from "./Logo";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { toggleSidebar } from "../features/user/userSlice";
+import { toggleSidebar, logoutUser } from "../features/user/userSlice";
 
 const Navbar = () => {
+  const [ showLogout, setShowLogout ] = useState(false);
   const { user } = useSelector((store) => store.user);
   const dispatch = useDispatch();
 
@@ -25,14 +26,14 @@ const Navbar = () => {
           <h3 className="logo-text">dashboard</h3>
         </div>
         <div className="btn-container">
-          <button type="button" className="btn" onClick={() => console.log('toggle logout dropdown')}>
+          <button type="button" className="btn" onClick={() => setShowLogout(!showLogout)}>
             <FaUserCircle />
             {user?.name}
             <FaCaretDown />
           </button>
 
-          <div className="dropdown show-dropdown">
-            <button type="button" className="dropdown-btn" onClick={() => console.log('logout user')}>
+          <div className={showLogout ? 'dropdown show-dropdown' : 'dropdown'}>
+            <button type="button" className="dropdown-btn" onClick={() => dispatch(logoutUser())}>
               logout
             </button>
           </div>
