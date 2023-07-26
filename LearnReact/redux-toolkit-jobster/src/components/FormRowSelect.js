4a76749e7176546c4f20a05478a8f77e2bf0a324
Anamarija <97092170+anamarijakukulj@users.noreply.github.com>
2023-07-26 15:04:11 +0200

    Update FormRowSelect.js

--- LearnReact/redux-toolkit-jobster/src/components/FormRowSelect.js
+++ LearnReact/redux-toolkit-jobster/src/components/FormRowSelect.js
@@ -11,7 +11,7 @@ const FormRowSelect = ({ labelText, name, value, handleChange, list }) => {
         onChange={handleChange}
         className="form-select"
       >
-        {list.map((index, itemValue) => {
+        {list.map((itemValue, index) => {
           return (
             <option key={index} value={itemValue}>
               {itemValue}
