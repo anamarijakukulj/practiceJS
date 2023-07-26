aeb4e55235a5271b822dc741e5dbc092c38b3993
Anamarija <97092170+anamarijakukulj@users.noreply.github.com>
2023-07-26 19:37:10 +0200

    Update index.js

--- LearnReact/redux-toolkit-jobster/src/components/index.js
+++ LearnReact/redux-toolkit-jobster/src/components/index.js
@@ -4,5 +4,8 @@ import FormRowSelect from "./FormRowSelect";
 import Navbar from "./Navbar";
 import SmallSidebar from "./SmallSidebar";
 import BigSidebar from "./BigSidebar";
+import SearchContainer from "./SearchContainer";
+import JobsContainer from "./JobsContainer";
+import Job from "./Job";
 
-export { Logo, FormRow, FormRowSelect, Navbar, SmallSidebar, BigSidebar };
+export { Logo, FormRow, FormRowSelect, Navbar, SmallSidebar, BigSidebar, SearchContainer, JobsContainer, Job };
