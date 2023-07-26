366ab49439737a9d7cdeddc7cbc0470f9fb44187
Anamarija <97092170+anamarijakukulj@users.noreply.github.com>
2023-07-26 15:07:23 +0200

    Add jobSlice actions

--- LearnReact/redux-toolkit-jobster/src/pages/dashboard/AddJob.js
+++ LearnReact/redux-toolkit-jobster/src/pages/dashboard/AddJob.js
@@ -2,6 +2,12 @@ import { FormRow, FormRowSelect } from "../../components";
 import Wrapper from "../../assets/wrappers/DashboardFormPage";
 import { useSelector, useDispatch } from "react-redux";
 import { toast } from "react-toastify";
+import {
+  handleChange,
+  clearValues,
+  createJob,
+} from "../../features/job/jobSlice";
+import { useEffect } from "react";
 
 const AddJob = () => {
   const {
@@ -17,6 +23,9 @@ const AddJob = () => {
     editJobId,
   } = useSelector((store) => store.job);
 
+  const { user } = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
@@ -24,13 +33,23 @@ const AddJob = () => {
       toast.error("Please fill out all fields!");
       return;
     }
+    dispatch(createJob({ position, company, jobLocation, jobType, status }));
   };
 
   const handleJobInput = (e) => {
     const name = e.target.name;
     const value = e.target.value;
-    console.log(name, value);
+    dispatch(handleChange({ name, value }));
   };
+
+  useEffect(() => {
+    dispatch(
+      handleChange({
+        name: "jobLocation",
+        value: user.location,
+      })
+    );
+  });
 
   return (
     <Wrapper>
@@ -81,7 +100,7 @@ const AddJob = () => {
             <button
               type="button"
               className="btn btn-block clear-btn"
-              onClick={() => console.log("clear values")}
+              onClick={() => dispatch(clearValues())}
             >
               clear
             </button>
