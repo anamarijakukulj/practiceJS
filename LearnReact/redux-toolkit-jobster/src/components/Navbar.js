5e630d611bd4e965dd3d76f24de641bd8d66d15c
Anamarija <97092170+anamarijakukulj@users.noreply.github.com>
2023-07-26 15:04:55 +0200

    Add logging out msg

--- LearnReact/redux-toolkit-jobster/src/components/Navbar.js
+++ LearnReact/redux-toolkit-jobster/src/components/Navbar.js
@@ -1,18 +1,18 @@
 import Wrapper from "../assets/wrappers/Navbar";
-import {FaAlignLeft, FaUserCircle, FaCaretDown} from "react-icons/fa";
+import { FaAlignLeft, FaUserCircle, FaCaretDown } from "react-icons/fa";
 import Logo from "./Logo";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { toggleSidebar, logoutUser } from "../features/user/userSlice";
 
 const Navbar = () => {
-  const [ showLogout, setShowLogout ] = useState(false);
+  const [showLogout, setShowLogout] = useState(false);
   const { user } = useSelector((store) => store.user);
   const dispatch = useDispatch();
 
   const toggle = () => {
     dispatch(toggleSidebar());
-  }
+  };
 
   return (
     <Wrapper>
@@ -26,14 +26,22 @@ const Navbar = () => {
           <h3 className="logo-text">dashboard</h3>
         </div>
         <div className="btn-container">
-          <button type="button" className="btn" onClick={() => setShowLogout(!showLogout)}>
+          <button
+            type="button"
+            className="btn"
+            onClick={() => setShowLogout(!showLogout)}
+          >
             <FaUserCircle />
             {user?.name}
             <FaCaretDown />
           </button>
 
-          <div className={showLogout ? 'dropdown show-dropdown' : 'dropdown'}>
-            <button type="button" className="dropdown-btn" onClick={() => dispatch(logoutUser())}>
+          <div className={showLogout ? "dropdown show-dropdown" : "dropdown"}>
+            <button
+              type="button"
+              className="dropdown-btn"
+              onClick={() => dispatch(logoutUser("Logging out..."))}
+            >
               logout
             </button>
           </div>
@@ -41,6 +49,6 @@ const Navbar = () => {
       </div>
     </Wrapper>
   );
-}
+};
 
 export default Navbar;
