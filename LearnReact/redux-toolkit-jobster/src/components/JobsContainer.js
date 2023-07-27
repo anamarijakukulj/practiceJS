32a8245a26c68de9d929e3455c989b272799918e
Anamarija <97092170+anamarijakukulj@users.noreply.github.com>
2023-07-27 11:10:01 +0200

    Add a structure

--- LearnReact/redux-toolkit-jobster/src/components/JobsContainer.js
+++ LearnReact/redux-toolkit-jobster/src/components/JobsContainer.js
@@ -1,5 +1,41 @@
+import { useEffect } from "react";
+import Job from "./Job";
+import Loading from "./Loading";
+import Wrapper from "../assets/wrappers/JobsContainer";
+import { useSelector, useDispatch } from "react-redux";
+import { getAllJobs } from "../features/allJobs/allJobsSlice";
+
 const JobsContainer = () => {
-  return <h1>jobs container</h1>
-}
+  const { jobs, isLoading } = useSelector((store) => store.allJobs);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getAllJobs());
+  }, []);
+
+  if (isLoading) {
+    return <Loading center />;
+  }
+
+  if (jobs.length === 0) {
+    return (
+      <Wrapper>
+        <h2>No jobs to display...</h2>
+      </Wrapper>
+    );
+  }
+
+  return (
+    <Wrapper>
+      <h5>jobs info</h5>
+
+      <div className="jobs">
+        {jobs.map((job) => {
+          return <Job key={job._id} {...job} />;
+        })}
+      </div>
+    </Wrapper>
+  );
+};
 
 export default JobsContainer;
