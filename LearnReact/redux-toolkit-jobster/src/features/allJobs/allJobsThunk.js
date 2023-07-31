eb8a3d5aaaf2bc00fd45377b26979ffe27d9df86
Anamarija <97092170+anamarijakukulj@users.noreply.github.com>
2023-07-31 10:17:25 +0200

    Add getAllJobsThunk and ShowStatsThunk

--- LearnReact/redux-toolkit-jobster/src/features/allJobs/allJobsThunk.js
+++ LearnReact/redux-toolkit-jobster/src/features/allJobs/allJobsThunk.js
@@ -1,5 +1,29 @@
-const allJobsThunk = () => {
-  return <h2>thunk</h2>
+import customFetch, { checkForUnauthorizedResponse } from '../../utils/axios';
+
+export const getAllJobsThunk = async (_, thunkAPI) => {
+  const { page, search, searchStatus, searchType, sort } =
+    thunkAPI.getState().allJobs;
+
+  let url = `/jobs?status=${searchStatus}&jobType=${searchType}&sort=${sort}&page=${page}`;
+  
+  if (search) {
+    url = url + `&search=${search}`;
+  }
+  
+  try {
+    const resp = await customFetch.get(url);
+    return resp.data;
+  } catch (error) {
+    return checkForUnauthorizedResponse(error, thunkAPI);
+  }
 };
 
-export default allJobsThunk;
+export const showStatsThunk = async (_, thunkAPI) => {
+  try {
+    const resp = await customFetch.get('/jobs/stats');
+
+    return resp.data;
+  } catch (error) {
+    return checkForUnauthorizedResponse(error, thunkAPI);
+  }
+};
